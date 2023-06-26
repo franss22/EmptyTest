0a604a32df2a3bb622215aa59cf41deb749f3a34
Francisco Ortiz <52766048+franss22@users.noreply.github.com>
2023-06-26 12:00:42 -0400

    Update analyzer category

--- EmptyTest/EmptyTest/EmptyTestAnalyzer.cs
+++ EmptyTest/EmptyTest/EmptyTestAnalyzer.cs
@@ -23,7 +23,7 @@ namespace EmptyTest
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private const string Category = "Test Smells";
 
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
 
